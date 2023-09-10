aadb7125941a371fdb73e27561b1c8a356a22ffc
kimidang26 <dangkimberly26@gmail.com>
2023-09-09 21:45:49 -0700

    Initial commit

---
+++ src/main/java/dev/kimberly26/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.kimberly26.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kimberly26/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kimberly26.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
